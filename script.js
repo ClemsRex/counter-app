fb0a3ae50d565229a31414a1fb1f1d37ee7ed137
ClemsRex <clementchukwu351@gmail.com>
2024-07-16 19:58:45 +0100

    complete counter app

---
+++ script.js
@@ -0,0 +1,116 @@
+const count = document.getElementById('count');
+let counter = 0;
+
+function increaseBtn() {
+    counter = counter + 1;
+    count.innerHTML = counter;
+};
+
+function decreaseBtn() {
+    counter = counter -1;
+    count.textContent = counter;
+};
+
+function reset() {
+    counter = 0;
+    count.innerHTML = counter;
+};
+
+function doubleX2() {
+    counter = counter * 2;
+    count.innerHTML = counter;
+};
+
+function square() {
+    counter = counter * counter;
+    count.innerHTML = counter;
+};
+
+function times5() {
+    counter = counter * 5;
+    count.innerHTML = counter;
+}
+
+function cube() {
+    counter = counter **3;
+    count.innerHTML = counter;
+};
+
+function times10() {
+    counter = counter * 10;
+    count.innerHTML = counter;
+};
+
+function times15() {
+    counter = counter * 15;
+    count.innerHTML = counter;
+};
+
+function squareRoot() {
+    counter = Math.sqrt(counter);
+    count.innerHTML = counter;
+};
+
+
+function times25() {
+    counter = counter * 25;
+    count.innerHTML = counter;
+};
+
+
+function times3() {
+    counter = counter * 3;
+    count.innerHTML = counter;
+};
+
+
+function times7() {
+    counter = counter * 7;
+    count.innerHTML = counter;
+};
+
+
+function times8() {
+    counter = counter * 8;
+    count.innerHTML = counter;
+};
+function times9() {
+    counter = counter * 9;
+    count.innerHTML = counter;
+};
+
+
+function times12() {
+    counter = counter * 12;
+    count.innerHTML = counter;
+};
+
+
+function times50() {
+    counter = counter * 50;
+    count.innerHTML = counter;
+};
+
+
+function plus4() {
+    counter = counter + 4;
+    count.innerHTML = counter;
+};
+
+
+function divideby5() {
+    counter = counter / 5;
+    count.innerHTML = counter;
+};
+
+
+function divideby15() {
+    counter = counter / 15;
+    count.innerHTML = counter;
+};
+
+
+function divideby35() {
+    counter = counter / 35;
+    count.innerHTML = counter;
+};
